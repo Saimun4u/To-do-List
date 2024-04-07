2e8be7c2ee04958ce85e4e9c89745baf936bb1a1
Saimun Hassan <saimunhas@gmail.com>
2024-04-07 16:59:49 -0400

    Added active toggle functionality for the add button

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputBox = document.querySelector('.inputField input');
+const addBtn = document.querySelector('.inputField button');
+
+inputBox.onkeyup = () => {
+  let userData = inputBox.value; //getting user entered value
+  if (userData.trim() != 0) {
+    addBtn.classList.add('active');
+  } else {
+    addBtn.classList.remove('active');
+  }
+};
