f22b50411d053e105574aa78cc162e2e880d6d46
Saimun Hassan <saimunhas@gmail.com>
2024-04-07 17:55:07 -0400

    Added local storage

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ const addBtn = document.querySelector('.inputField button');
 inputBox.onkeyup = () => {
   let userData = inputBox.value; //getting user entered value
   if (userData.trim() != 0) {
-    addBtn.classList.add('active');
+    //if user values aren't only spaces
+    addBtn.classList.add('active'); //activate the add button
   } else {
-    addBtn.classList.remove('active');
+    addBtn.classList.remove('active'); //unactivate the add button
   }
+};
+
+// Set local storage once add button is clicked
+
+addBtn.onclick = () => {
+  let userData = inputBox.value; //getting user entered value
+  let getLocalStorage = localStorage.getItem('New Todo'); //getting localstorage
+  if (getLocalStorage == null) {
+    //if localstorage is null
+    listArr = []; // creating blank array
+  } else {
+    listArr = JSON.parse(getLocalStorage); //transforming json string into a js object
+  }
+  listArr.push(userData); // pushing or adding user data
+  localStorage.setItem('New Todo', JSON.stringify(listArr)); // transforming js object into a json string
 };
