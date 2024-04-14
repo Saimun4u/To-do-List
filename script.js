f8bef04072d7518462bb40248824bbc0748dc6f9
Saimun Hassan <saimunhas@gmail.com>
2024-04-13 21:08:02 -0400

    Added fucntionality for items to populate and show in the page

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.querySelector('.inputField input');
 const addBtn = document.querySelector('.inputField button');
+const toList = document.querySelector('.todoList');
 
 inputBox.onkeyup = () => {
   let userData = inputBox.value; //getting user entered value
@@ -15,6 +16,7 @@ inputBox.onkeyup = () => {
 
 addBtn.onclick = () => {
   let userData = inputBox.value; //getting user entered value
+  // localStorage.clear(); //reset local storage
   let getLocalStorage = localStorage.getItem('New Todo'); //getting localstorage
   if (getLocalStorage == null) {
     //if localstorage is null
@@ -24,4 +26,22 @@ addBtn.onclick = () => {
   }
   listArr.push(userData); // pushing or adding user data
   localStorage.setItem('New Todo', JSON.stringify(listArr)); // transforming js object into a json string
+  showTasks(); //calling showTasks function
 };
+
+function showTasks() {
+  let getLocalStorage = localStorage.getItem('New Todo'); //getting localstorage
+  if (getLocalStorage == null) {
+    //if localstorage is null
+    listArr = []; // creating blank array
+  } else {
+    listArr = JSON.parse(getLocalStorage); //transforming json string into a js object
+  }
+  let newLiTag = '';
+  listArr.forEach((element, index) => {
+    newLiTag += `<li> ${element}
+    <span><i class="fas fa-trash"></i></span>
+  </li>`;
+  });
+  toList.innerHTML = newLiTag; // adding an new li tagg inside ul tag
+}
